72ca2c0008aeaed7060b4b54f2674e3e2569564c
FaizanMuhammadSE <bsef18m023@pucit.edu.pk>
2022-04-23 18:06:12 +0500

    files changed for getting json

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 import express from "express";
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 2000;
+import cors from "cors";
+const corsOptions = {
+	origin: "*",
+	credentials: true, //access-control-allow-credentials:true
+	optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions)); // Use this after the variable declaration
 
 app.get("/", (req, res) => {
 	res.send("I will test servers");
@@ -15,7 +23,7 @@ app.get("/signup", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-	res.json({ name: "Faizan Muhammad", semester: "8th Semester" });
+	res.json([{ name: "Faizan Muhammad", semester: "8th Semester" }]);
 });
 
 app.use("/static", express.static("src"));
