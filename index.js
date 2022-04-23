d80ad0099a30098bc18fe7151cd2e62dd0d13dfa
FaizanMuhammadSE <bsef18m023@pucit.edu.pk>
2022-04-23 12:16:57 +0500

    Server Code added

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+	res.send("I will test servers");
+});
+
+app.get("/login", (req, res) => {
+	res.send("This is login Page");
+});
+
+app.get("/signup", (req, res) => {
+	res.send("This is signup Page");
+});
+
+app.get("/json", (req, res) => {
+	res.json({ name: "Faizan Muhammad", semester: "8th Semester" });
+});
+
+app.use("/static", express.static("src"));
+
+app.listen(port, () => {
+	console.log(`Example app listening on port http://localhost:${port}`);
+});
